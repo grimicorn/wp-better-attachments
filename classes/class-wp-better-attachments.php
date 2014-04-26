d2f3fc0479d52bb9d23e793d49a04cfc7d92fcdd
Dan Holloran <dholloran@matchboxdesigngroup.com>
2014-04-26 07:31:09 -0500

    Made meta box id public for use in settings

--- classes/class-wp-better-attachments.php
+++ classes/class-wp-better-attachments.php
@@ -25,7 +25,7 @@ if ( ! class_exists( 'WP_Better_Attachments' ) ) {
 		 *
 		 * @var   string
 		 */
-		protected $meta_box_id = 'wpba_meta_box';
+		public $meta_box_id = 'wpba_meta_box';
 
 
 		/**
@@ -129,7 +129,7 @@ if ( ! class_exists( 'WP_Better_Attachments' ) ) {
 			} // if()
 
 			wp_enqueue_style( 'wpba_admin_css', WPBA_URL . '/assets/css/dist/wpba-admin.8f34.min.css', array(), null, 'all' );
-			wp_register_script( 'wpba_admin_js', WPBA_URL . '/assets/js/dist/wpba-admin.2b61.min.js', array( 'jquery-ui-sortable' ), null, true );
+			wp_register_script( 'wpba_admin_js', WPBA_URL . '/assets/js/dist/wpba-admin.65d4.min.js', array( 'jquery-ui-sortable' ), null, true );
 			wp_enqueue_script( 'wpba_admin_js' );
 		} // enqueue_admin_assets()
 	} // WP_Better_Attachments()
