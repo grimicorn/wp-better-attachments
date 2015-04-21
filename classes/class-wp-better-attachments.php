7758fb5d770b56d0c95db68b9aa4fe72ca18d90d
Dan Holloran <dtholloran@gmail.com>
2015-04-20 21:20:40 -0500

    Adds a few required params

--- classes/class-wp-better-attachments.php
+++ classes/class-wp-better-attachments.php
@@ -40,6 +40,45 @@ if ( ! class_exists( 'WP_Better_Attachments' ) ) {
 
 
 		/**
+		 * The ID for the meta box.
+		 *
+		 * @todo  allow for multiple meta boxes on a page.
+		 *
+		 * @since 1.4.0
+		 *
+		 * @var   string
+		 */
+		public $meta_box_id = 'wpba_meta_box';
+
+
+
+		/**
+		 * The meta key for allowing attachments to be added to multiple posts.
+		 *
+		 * @todo  allow for multiple meta boxes on a page.
+		 *
+		 * @since 1.4.0
+		 *
+		 * @var   string
+		 */
+		public $attachment_multi_meta_key;
+
+
+
+		/**
+		 * The meta key for attachments menu order when added to multiple posts.
+		 *
+		 * @todo  allow for multiple meta boxes on a page.
+		 *
+		 * @since 1.4.0
+		 *
+		 * @var   string
+		 */
+		public $attachment_multi_menu_order_meta_key;
+
+
+
+		/**
 		 * Class constructor
 		 *
 		 * @since   2.0.0
